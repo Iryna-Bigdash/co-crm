114b4382836ca6cfa7d97acc653645566e4ca78c
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-03-25 20:41:00 +0000

    added  tailwind

--- co-crm/src/app/page.tsx
+++ co-crm/src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+      <h1 className="text-amber-500">Hello</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
