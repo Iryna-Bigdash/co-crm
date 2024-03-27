89339f3906e7273c9740adda8a8c7e8a2c284fd4
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-03-27 13:53:15 +0000

    added add company modal
    added add company modal and basic style for it

---
+++ co-crm/src/app/components/add-company-button.tsx
@@ -0,0 +1,40 @@
+// 'use client';
+
+// import React from 'react';
+// import { useRouter } from 'next/navigation';
+// import Button from './button';
+
+// export default function AddCompanyButton() {
+//   const router = useRouter();
+
+//   return (
+//     <Button onClick={() => router.push('/companies/new', { scroll: false })}>
+//       Add company
+//     </Button>
+//   );
+// }
+
+'use client';
+
+import React, { useState } from 'react';
+import Button from './button';
+import CompanyFormModal from './company-form-modal';
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false)
+
+  return (
+    <>
+    <Button onClick={() => setShow(true)}>
+      Add company
+    </Button>
+    <CompanyFormModal
+    onSubmit={console.log}
+    show={show}
+    onClose={() => setShow(false)}
+    />
+    </>
+    
+  );
+}
+
---
+++ co-crm/src/app/components/logo-uploader.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+export interface LogoUploaderProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  label?: string;
+  square?: boolean;
+}
+
+export default function LogoUploader({
+  square,
+  label,
+  placeholder,
+  id,
+  ...rest
+}: LogoUploaderProps) {
+  return (
+    <div
+      className={clsx(
+        'flex mb-3',
+        !square && 'gap-10',
+        square && 'gap-2 flex-col',
+      )}
+    >
+      {label && <p className="text-base color-gray-900">{label}</p>}
+      <label
+        htmlFor={id}
+        className={clsx(
+          'flex flex-col items-center justify-center h-40 bg-white border border-slate-900 border-dashed cursor-pointer',
+          !square && 'w-40 rounded-full',
+          square && 'w-full',
+        )}
+      >
+        {/* <Image
+          className="mb-1"
+          width={48}
+          height={48}
+          src="/icons/upload.svg"
+          alt="upload"
+        /> */}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+</svg>
+
+        {placeholder && (
+          <p className="text-base text-gray-500">{placeholder}</p>
+        )}
+        <input
+          {...rest}
+          id={id}
+          type="file"
+          accept="image/*"
+          className="hidden"
+        />
+      </label>
+    </div>
+  );
+}
