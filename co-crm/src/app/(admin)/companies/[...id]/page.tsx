444a0216bbd63769776e89c2b1158fa78b844f64
Iryna B <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-03-30 22:10:31 +0000

    added error-handling

--- co-crm/src/app/(admin)/companies/[...id]/page.tsx
+++ co-crm/src/app/(admin)/companies/[...id]/page.tsx
@@ -1,10 +1,27 @@
+'use client';
+import React, { useEffect } from 'react';
 import Header from '@/app/components/header';
+import { notFound } from 'next/navigation';
+
 
 export interface PageProps {
   params: { id: string[] };
 }
 
 export default function Page({ params }: PageProps) {
+
+  useEffect(() => {
+    
+    params.id.forEach((idString) => {
+      const id = Number.parseInt(idString);
+
+      if (Number.isNaN(id)) {
+        notFound();
+        return;
+      }
+    });
+  }, [params.id]);
+
   return (
     <>
       <Header>Companies {String(params.id)}</Header>
--- co-crm/src/app/(admin)/dashboard/page.tsx
+++ co-crm/src/app/(admin)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import MagicButton from "@/app/components/magic-button";
+
 export interface PageProps {
 
 }
@@ -6,6 +8,7 @@ export default function Page({}: PageProps) {
     return(
         <main>
             <h1 className="text-xl">Dashboard Page</h1>
+            <MagicButton />
         </main>
     )
 }
