6372c38dd53702c7ed5438400492c76f61bd4f77
Iryna B. <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-06-13 17:20:09 +0100

    added exporting data from github

--- co-crm/src/app/components/header.tsx
+++ co-crm/src/app/components/header.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import Image from 'next/image';
+// import React from 'react';
+// import Image from 'next/image';
+// import Link from 'next/link';
 
-export interface HeaderProps {
-  children: React.ReactNode;
-}
+// export interface HeaderProps {
+//   children: React.ReactNode;
+// }
 
-export default function Header({ children }: HeaderProps) {
-  return (
-    <header className="flex items-center gap-5 py-6	px-10 border-b border-gray-300">
-      <h1 className="flex-1 text-3xl font-semibold text-gray-900">
-        {children}
-      </h1>
-      <div className="w-px self-stretch bg-gray-300" />
-      <div className="flex gap-3">
-        <Image width={44} height={44} src="/images/avatar1.png" alt="avatar" />
-        <div>
-          <p className="text-base	font-semibold text-gray-900">Iryna Bigdash</p>
-          <p className="text-sm	font-light text-gray-900">imbigdash@gmail.com</p>
-        </div>
-      </div>
-    </header>
-  );
-}
+// export default function Header({ children }: HeaderProps) {
+//   return (
+//     <header className="flex items-center gap-5 py-6	px-10 border-b border-gray-300">
+//       <h1 className="flex-1 text-3xl font-semibold text-gray-900">
+//         {children}
+//       </h1>
+//       <div className="w-px self-stretch bg-gray-300" />
+//       <div className="flex gap-3">
+//         <Image width={44} height={44} src="/images/avatar1.png" alt="avatar" />
+//         <div>
+//           <p className="text-base	font-semibold text-gray-900">Iryna Bigdash</p>
+//           <p className="text-sm	font-light text-gray-900">imbigdash@gmail.com</p>
+//         </div>
+//         <div>
+//           <Link href={'http://localhost:3000/api/auth/signout'}>Sign out</Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
+  'use client';
+
+  import React from 'react';
+  import Image from 'next/image';
+  import Link from 'next/link';
+  import { useSession, signOut } from 'next-auth/react';
+  
+  export interface HeaderProps {
+    children: React.ReactNode;
+  }
+  
+  export default function Header({ children }: HeaderProps) {
+    const { data: session, status } = useSession();
+  
+    console.log('hi', session);
+    const userImage = session?.user?.image ? session.user.image : '/images/avatar1.png';
+  
+    if (status === 'loading') {
+      return <p>Loading...</p>;
+    }
+  
+    return (
+      <header className="flex items-center gap-5 py-6 px-10 border-b border-gray-300">
+        <h1 className="flex-1 text-3xl font-semibold text-gray-900">
+          {children}
+        </h1>
+        <div className="w-px self-stretch bg-gray-300" />
+        <div className="flex gap-3">
+            {session ? (
+              <>
+                  <Image width={44} height={44} src={userImage} alt="avatar" className='rounded-full' />
+                  <div>
+                <p className="text-base font-semibold text-gray-900">{session.user?.name}</p>
+                <p className="text-sm font-light text-gray-900">{session.user?.email}</p>
+                </div>
+                <button onClick={() => signOut()} className="text-blue-500">Sign out</button>
+              </>
+            ) : (
+              <Link href="/api/auth/signin">Sign in</Link>
+            )}
+          </div>
+      </header>
+    );
+  }
+  
--- co-crm/src/app/layout.tsx
+++ co-crm/src/app/layout.tsx
@@ -1,25 +1,53 @@
+// import React from 'react';
+// import { Plus_Jakarta_Sans } from 'next/font/google';
+// import './globals.css';
+// import Providers from './components/providers';
+
+// const font = Plus_Jakarta_Sans({ subsets: ['latin']});
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={font.className}>
+//         <Providers>
+//         {children}
+//         </Providers>
+//         </body>
+//     </html>
+//   );
+// } 
+
 import React from 'react';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
-import Providers from './components/providers';
+import AuthProviders from './components/auth-providers';
 
-const font = Plus_Jakarta_Sans({ subsets: ['latin']});
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'CRM system',
+  description: 'Developed by Iryna Bigdash'
+  }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Providers>
-        {children}
-        </Providers>
-        </body>
+        <AuthProviders>
+          {children}
+        </AuthProviders>
+      </body>
     </html>
   );
-} 
+}
 
 
 
