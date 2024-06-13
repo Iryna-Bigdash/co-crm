3e7036db2dbdcd0565990dcb85ab79a44a4ca03a
Iryna B. <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-06-13 17:21:58 +0100

    Update header.tsx

--- co-crm/src/app/components/header.tsx
+++ co-crm/src/app/components/header.tsx
@@ -27,48 +27,60 @@
 //   );
 // }
 
-  'use client';
+'use client';
 
-  import React from 'react';
-  import Image from 'next/image';
-  import Link from 'next/link';
-  import { useSession, signOut } from 'next-auth/react';
-  
-  export interface HeaderProps {
-    children: React.ReactNode;
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/react';
+
+export interface HeaderProps {
+  children: React.ReactNode;
+}
+
+export default function Header({ children }: HeaderProps) {
+  const { data: session, status } = useSession();
+
+  const userImage = session?.user?.image
+    ? session.user.image
+    : '/images/avatar1.png';
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
   }
-  
-  export default function Header({ children }: HeaderProps) {
-    const { data: session, status } = useSession();
-  
-    console.log('hi', session);
-    const userImage = session?.user?.image ? session.user.image : '/images/avatar1.png';
-  
-    if (status === 'loading') {
-      return <p>Loading...</p>;
-    }
-  
-    return (
-      <header className="flex items-center gap-5 py-6 px-10 border-b border-gray-300">
-        <h1 className="flex-1 text-3xl font-semibold text-gray-900">
-          {children}
-        </h1>
-        <div className="w-px self-stretch bg-gray-300" />
-        <div className="flex gap-3">
-            {session ? (
-              <>
-                  <Image width={44} height={44} src={userImage} alt="avatar" className='rounded-full' />
-                  <div>
-                <p className="text-base font-semibold text-gray-900">{session.user?.name}</p>
-                <p className="text-sm font-light text-gray-900">{session.user?.email}</p>
-                </div>
-                <button onClick={() => signOut()} className="text-blue-500">Sign out</button>
-              </>
-            ) : (
-              <Link href="/api/auth/signin">Sign in</Link>
-            )}
-          </div>
-      </header>
-    );
-  }
-  
+
+  return (
+    <header className="flex items-center gap-5 py-6 px-10 border-b border-gray-300">
+      <h1 className="flex-1 text-3xl font-semibold text-gray-900">
+        {children}
+      </h1>
+      <div className="w-px self-stretch bg-gray-300" />
+      <div className="flex gap-3">
+        {session ? (
+          <>
+            <Image
+              width={44}
+              height={44}
+              src={userImage}
+              alt="avatar"
+              className="rounded-full"
+            />
+            <div>
+              <p className="text-base font-semibold text-gray-900">
+                {session.user?.name}
+              </p>
+              <p className="text-sm font-light text-gray-900">
+                {session.user?.email}
+              </p>
+            </div>
+            <button onClick={() => signOut()} className="text-blue-500">
+              Sign out
+            </button>
+          </>
+        ) : (
+          <Link href="/api/auth/signin">Sign in</Link>
+        )}
+      </div>
+    </header>
+  );
+}
