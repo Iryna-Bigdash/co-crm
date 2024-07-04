e22ad69c759e42d4e498be68933d22ab173e4f85
Iryna B. <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-07-04 18:34:08 +0100

    remove iSearchInput component

--- co-crm/src/app/(admin)/companies/@toolbar/[id]/page.tsx
+++ co-crm/src/app/(admin)/companies/@toolbar/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
  import Toolbar from '@/app/components/toolbar';
- import SearchInput from '@/app/components/search-input';
+//  import SearchInput from '@/app/components/search-input';
  import AddPromotionButton from '@/app/components/add-promotion-button';
 
  export interface PageProps {
@@ -10,7 +10,7 @@ import React from 'react';
  export default function Page({ params }: PageProps) {
    return (
      <Toolbar action={<AddPromotionButton companyId={params.id} />}>
-       <SearchInput />
+       {/* <SearchInput /> */}
      </Toolbar>
    );
  }
