73d39102cd2deab175eb2e45cf8848849fa21110
Iryna B. <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-07-23 14:39:55 +0100

    remove console.log(profile) for login by github

--- co-crm/src/app/api/auth/[...nextauth]/options.ts
+++ co-crm/src/app/api/auth/[...nextauth]/options.ts
@@ -14,7 +14,7 @@ export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       profile(profile: GithubProfile) {
-        console.log(profile);
+        // console.log(profile);
         return {
           ...profile,
           role: profile.role ?? 'user',
