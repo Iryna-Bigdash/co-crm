cf148cb39bca37a61b46eff9356e9c999f84c7ca
Iryna B. <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-08-04 21:09:26 +0100

    added link to ln

--- src/app/components/greeting.tsx
+++ src/app/components/greeting.tsx
@@ -6,10 +6,10 @@ export interface GreetingProps {};
 
 const Greeting: React.FC<GreetingProps> = () => {
   return (
-    <div>
+    <>
           <h1 className="animate-changeColor text-3xl font-bold tracking-tight text-white sm:text-5xl">CRM system for your business</h1>
 
-      </div>
+      </>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,24 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="bg-slate-800 min-h-screen flex flex-col items-center justify-center gap-20">
+    <main className="bg-slate-800 min-h-screen flex flex-col justify-between py-4">
+      <header className='py-10'> 
       <Image
-          className="py-8 mx-auto"
-          width={350}
-          height={185}
-          src="/icons/logo.svg"
-          alt="logo"
-        />
+        className="py-8 mx-auto"
+        width={350}
+        height={185}
+        src="/icons/logo.svg"
+        alt="logo"
+      />
+      </header>
+    <section className='py-4 flex flex-col items-center justify-center gap-20'>
       <Greeting />
       <Link href="/dashboard" className="text-lg text-white sm:text-3xl">Get started</Link>
-    </main>
+    </section>
+    <footer className='flex justify-center py-4'>
+      <p className='text-gray-200'>Developed by <Link href="https://www.linkedin.com/in/iryna-bigdash" target="_blank" className='underline'>Iryna Bigdash</Link></p>
+    </footer>
+  </main>
   );
 }
 
